c9b5e89d99bb63d61b5cea9f84bbac8a516c7fca
SaraChayani <selftaughtshaygani@gmail.com>
2024-06-01 17:13:15 +0700

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,20 @@
 
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
-},
-{
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  },
+  {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 //Adding items to cart id the items doesn't exist, otherwise, increase the quantity of the item if it already exists.
 export function addToCart(productId) {
   let matchingItem;
@@ -26,6 +34,7 @@ export function addToCart(productId) {
       quantity
     });
   }
+  saveToStorage();
 }
 
 //Deleting items from the cart and generating updated HTML with updated Cart.
@@ -37,22 +46,5 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
-  //console.log(cart);
-  // //Finding the items to delete based on dataset product id peovided by the delete clicked link.
-  // console.log(cart);
-  // let matchingItemIndex = -1;
-  // cart.forEach((cartItem, index) => {
-  //   //console.log('inside cart.foreach')
-  //   if (cartItem.productId === productId) {
-  //     //console.log('inside if product id == productId')
-  //     matchingItemIndex = index;
-  //   }
-  // });
-  // //console.log(matchingItemIndex);
-  // //If a matching item has been found, get it's index and delete it from the cart then generate the updated cart again.
-  // if (matchingItemIndex >= 0) {
-  //   cart.splice(matchingItemIndex, 1);
-  //   console.log('deleted');
-  //   console.log(cart);
-  // }
+  saveToStorage();
 }

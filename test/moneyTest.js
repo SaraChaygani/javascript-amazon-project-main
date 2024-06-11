9fa2d70cf6e99c6588e1bab97e23f93be2885f71
SaraChayani <selftaughtshaygani@gmail.com>
2024-06-11 15:42:07 +0700

    Create automated test for formatCurrency

---
+++ test/moneyTest.js
@@ -0,0 +1,16 @@
+import { convertCurrency } from '../scripts/utils/utility.js';
+
+console.log('Converts cents into dollars');
+
+if (convertCurrency(2095) === '20.95') {
+  console.log('passed');
+}
+else {
+  console.log('failed');
+}
+
+console.log('works with 0');
+console.log(convertCurrency(0) === '0.00' ? 'passed' : 'failed');
+
+console.log('rounds up to the nearest cent')
+console.log(convertCurrency(2000.5) === '20.01' ? 'passed' : 'failed');

ef3dd2611afcacb129ed14df77c2ab5352d10111
SaraChayani <selftaughtshaygani@gmail.com>
2024-06-27 16:30:12 +0700

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/oderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import '../data/car.js';
-
+import '../data/backend-practice.js';
 renderOrderSummary();
 renderPaymentSummary();
 renderCheckoutHeader();

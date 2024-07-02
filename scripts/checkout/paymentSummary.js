782285486ddeb7679b858461ca1c58026874c351
SaraChayani <selftaughtshaygani@gmail.com>
2024-07-02 15:14:18 +0700

    Create order  using backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
 import { convertCurrency } from '../utils/utility.js';
+import { addOrder } from '../../data/orders.js';
 
 export function renderPaymentSummary() {
   let itemsTotalPrice = 0,
@@ -49,10 +50,33 @@ export function renderPaymentSummary() {
             <div class="payment-summary-money">$${convertCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
   `;
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order').addEventListener('click', async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+
+      const order = await response.json();
+      addOrder(order);
+    }
+
+    catch (error) {
+      console.log('Unexpectted error');
+    }
+
+    window.location.href = 'orders.html';
+  });
 }
